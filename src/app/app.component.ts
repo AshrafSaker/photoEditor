fb21211353bea7582663781610724f0ff97afcf7
Ashraf Saker <engashrafsaker@gmail.com>
2024-06-23 17:35:21 +0300

    added form & ngmodels and made requierd and vaild in CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
+import { FormsModule } from '@angular/forms';
 import {HeaderComponent} from '../header/header.component';
 import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,RouterOutlet],
+  imports: [HeaderComponent,RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -26,6 +28,14 @@ export class AppComponent {
 
   isHighlighted = true;
   myClasses = ['highlight', 'uppercase'];
+
+  products = ['apple', 'orange', 'banana'];
+  name = ' null';
+  password = '****';
+  username = '';
+  showName(){
+    alert(this.username);
+  }
 }
 
 
