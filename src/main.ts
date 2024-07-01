7fb0af764118aab5ced4083585e8bfe1c7440306
Ashraf Saker <engashrafsaker@gmail.com>
2024-07-01 21:59:37 +0300

    made Error page
    Apps need to handle incorrect URLs, to display a custom error page when the user attempts to navigate to an unknown path.
    
    For this, Angular provides wildcard routes, defined by two asterisks **

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import {provideRouter,Routes} from '@angular/router'
 import { HomeComponent } from './home/home.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { TestComponent } from './test/test.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'contacts', component: ContactsComponent },
   { path: 'test', component: TestComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 
