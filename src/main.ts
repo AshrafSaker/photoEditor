899add5bb2981b0d7f12aeb1f7ec81dc6f957053
Ashraf Saker <engashrafsaker@gmail.com>
2024-07-01 21:53:05 +0300

    default path
    In the router config, you can define the default path, using an empty set a quotes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import {provideRouter,Routes} from '@angular/router'
 import { HomeComponent } from './home/home.component';
@@ -7,6 +7,7 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'contacts', component: ContactsComponent },
   { path: 'test', component: TestComponent },
