8c36fd4cfabfcada4dbce9c6b589a69a1310b473
Ashraf Saker <engashrafsaker@gmail.com>
2024-07-01 22:20:42 +0300

    inject
    You can also navigate to routes using special navigation methods.
    
    For example, let's create a button in the HomeComponent and navigate to the Contact page when it's clicked.
    
    <button (click)="navigate()">Contacts</button>
    HTML
    And here is our navigate() method:
    
    CODE PLAYGROUND
    TS
    router = inject(Router);
    navigate() {
      this.router.navigateByUrl('/contacts');
    }
    Click to run
    We used the inject() method from @angular/core to get the router instance and used its <b>navigateByUrl()</b> method to navigate to the given path.

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  router = inject(Router);
+  navigate() {
+    this.router.navigateByUrl('/contacts');
+  }
 }
+
+
