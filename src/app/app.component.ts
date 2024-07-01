d0c122a6d106e1de50bf4756534619c255c2b208
Ashraf Saker <engashrafsaker@gmail.com>
2024-07-01 21:07:40 +0300

    translate code data
    translate data from app ts,css,html to test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 
-import { FormControl, FormsModule } from '@angular/forms';
+
 import {HeaderComponent} from '../header/header.component';
 import { Component} from '@angular/core';
 import { RouterOutlet, Routes } from '@angular/router';
-import  { ReactiveFormsModule } from '@angular/forms';
-import { FormGroup } from '@angular/forms';
-import { Validators } from '@angular/forms';
+
+
 import { RouterModule } from '@angular/router';
 
 
@@ -16,52 +15,12 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,RouterOutlet,FormsModule,ReactiveFormsModule,RouterModule],
+  imports: [HeaderComponent,RouterOutlet,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  login() {
 
-    alert("Welcome");
-  };
-
-  isRed = false;
-  upperClass = ['uppercase'];
-  change() {
-    this.isRed = !this.isRed;
-    console.log("changed");
-  };
-  imageURL = 'https://www.sololearn.com/images/tree.jpg';
-  imgAlt = 'Tree';
-
-  isHighlighted = true;
-  myClasses = ['highlight', 'uppercase'];
-
-  products = ['apple', 'orange', 'banana'];
-  name = '';
-  password = '';
-  username = '';
-  showName(){
-    console.log(this.username);
-  };
-
-  //formcontrol
-  formName = new FormControl('');
-  color = new FormControl('Red');
-  showFormControl(){
-    console.log(this.formName.value)
-    console.log(this.color.value)
-  };
-
-  //FormGroup
-  formLogin = new FormGroup({
-    user: new FormControl('',Validators.required),
-    pass: new FormControl(''),
-  });
-  groupForm(){
-    console.log(this.formLogin.value.user + '|'+this.formLogin.value.pass)
-  };
 }
 
 
--- src/test/test.component.ts
+++ src/test/test.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormControl, FormsModule } from '@angular/forms';
+import  { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule,],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
 export class TestComponent {
+  login() {
+
+    alert("Welcome");
+  };
+
+  isRed = false;
+  upperClass = ['uppercase'];
+  change() {
+    this.isRed = !this.isRed;
+    console.log("changed");
+  };
+  imageURL = 'https://www.sololearn.com/images/tree.jpg';
+  imgAlt = 'Tree';
+
+  isHighlighted = true;
+  myClasses = ['highlight', 'uppercase'];
+
+  products = ['apple', 'orange', 'banana'];
+  name = '';
+  password = '';
+  username = '';
+  showName(){
+    console.log(this.username);
+  };
+
+  //formcontrol
+  formName = new FormControl('');
+  color = new FormControl('Red');
+  showFormControl(){
+    console.log(this.formName.value)
+    console.log(this.color.value)
+  };
+
+  //FormGroup
+  formLogin = new FormGroup({
+    user: new FormControl('',Validators.required),
+    pass: new FormControl(''),
+  });
+  groupForm(){
+    console.log(this.formLogin.value.user + '|'+this.formLogin.value.pass)
+  };
 
 }
